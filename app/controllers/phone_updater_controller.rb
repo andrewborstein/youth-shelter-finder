1f20784ba1d5f6d87f5ceea6dcd4f48c03fc128e
gordonmotsinger <gordon.motsinger@gmail.com>
2015-03-31 09:03:47 -0400

    added PhoneUpdaterController and a few empty methods

---
+++ app/controllers/phone_updater_controller.rb
@@ -0,0 +1,16 @@
+class PhoneUpdaterController < ApplicationController
+
+    def index
+        @shelters = Shelter.all
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def delete
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331012154) do
+ActiveRecord::Schema.define(version: 20150331123933) do
+
+  create_table "phone_updaters", force: :cascade do |t|
+    t.string   "shelter_id"
+    t.string   "phone_number"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string   "name"
